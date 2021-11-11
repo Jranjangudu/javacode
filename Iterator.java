7f5f32e048ae1dea94f957a4f8f4f90cf28c62f9
Jranjangudu <9114338163gudu@gmail.com>
2021-11-11 23:03:41 +0530

    remove comment

--- Iterator.java
+++ Iterator.java
@@ -18,37 +18,37 @@ public class Iterator
 	           //System.out.println(it.next());
 	        }
 		    System.out.println(i); //[12,42] { 52 and 62 removed }
-// 		ArrayList<String> st = new ArrayList<String>();
-// 		st.add("simran");
-// 		st.add("jyoti");
-// 		st.add("sonam");
-// 		st.add("Guddu");
-// 		Iterator<String> it = st.iterator();
-// 		while(it.hasNext()){
+		ArrayList<String> st = new ArrayList<String>();
+		st.add("simran");
+		st.add("jyoti");
+		st.add("sonam");
+		st.add("Guddu");
+		Iterator<String> it = st.iterator();
+		while(it.hasNext()){
 	
+		System.out.println(it.next());
 // 		System.out.println(it.next());
-// // 		System.out.println(it.next());
-// 		}
-// 		System.out.println(it.next());
-// 		// creating Object
-// 		Persion p1 = new Persion();
-// 		Persion p2 = new Persion();
-// 		p1.name = "Simran";
-// 		p1.roll = 2020;
-// 		p1.walk();
-// // 		System.out.println(p1.name);
-//       p2.name = "Jyoti";
-//       p2.roll = 1212;
-//       p2.walk();
+		}
+		System.out.println(it.next());
+		// creating Object
+		Persion p1 = new Persion();
+		Persion p2 = new Persion();
+		p1.name = "Simran";
+		p1.roll = 2020;
+		p1.walk();
+// 		System.out.println(p1.name);
+      p2.name = "Jyoti";
+      p2.roll = 1212;
+      p2.walk();
 	}
 }
 
-// class Persion{
-//     String name;
-//     int roll;
+class Persion{
+    String name;
+    int roll;
     
-//     void walk(){
-//         System.out.println(name + " " + "is walk");
-//     }
-// }
+    void walk(){
+        System.out.println(name + " " + "is walk");
+    }
+}
 
--- Main.java
+++ Main.java
@@ -1,31 +1,31 @@
-// import java.util.Arrays;
+import java.util.Arrays;
 
-// import java.util.Scanner;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter your name");
-        // String name = sc.next();
-        // String total = "my name is " + name;
-        // System.out.println(total);
-        // int[] marks = new int[4];
-        // for (int i = 0; i < 4; i++) {
-        // marks[i] = i * 10;
-        // }
-        // System.out.println(marks[3]);
-        // Arrays.sort(marks);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.next();
+        String total = "my name is " + name;
+        System.out.println(total);
+        int[] marks = new int[4];
+        for (int i = 0; i < 4; i++) {
+        marks[i] = i * 10;
+        }
+        System.out.println(marks[3]);
+        Arrays.sort(marks);
 
-        // // Another way to create array using {};
-        // int[] data = { 45, 6, 8, 9, 46 };
-        // try {
-        // // System.out.println(data[10]);
-        // } catch (Exception e) {
-        //
-        // // System.out.println("Error occure");
-        // }
-        // Arrays.sort(data);
-        // System.out.println(data[0]);
-        // System.out.println(data.length);
+        // Another way to create array using {};
+        int[] data = { 45, 6, 8, 9, 46 };
+        try {
+        System.out.println(data[10]);
+        } catch (Exception e) {
+        
+        System.out.println("Error occure");
+        }
+        Arrays.sort(data);
+        System.out.println(data[0]);
+        System.out.println(data.length);
 
         int a = 10;
         int b = 20;
@@ -35,16 +35,16 @@ public class Main {
             System.out.println(false);
         }
 
-        // int i = 0;
-        // while (i <= 5) {
-        // if (i == 3) {
-        // continue;}
-        // // } else if (i == 4) {
-        // // break;
-        // // }
-        // System.out.println(i);
-        // i = i + 1;
+        int i = 0;
+        while (i <= 5) {
+        if (i == 3) {
+        continue;}
+        // } else if (i == 4) {
+        // break;
         // }
+        System.out.println(i);
+        i = i + 1;
+        }
 
         // constant
         final float PI = 3.14F; // constant value
